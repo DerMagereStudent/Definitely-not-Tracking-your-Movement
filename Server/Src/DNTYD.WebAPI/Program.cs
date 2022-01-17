80c1f78de475623edd6bff93e2ab12fceb55e6b2
DerMagereStudent <malo0466@gmail.com>
2022-01-17 16:55:19 +0100

    Server Backend - Src - Created Clean Architecture projects

---
+++ Server/Src/DNTYD.WebAPI/Program.cs
@@ -0,0 +1,34 @@
+namespace DNTYD.WebAPI;
+
+public static class Program {
+	public static void Main(string[] args) {
+		var builder = WebApplication.CreateBuilder(args);
+		Program.ConfigureServices(builder);
+		var app = builder.Build();
+		Program.ConfigurePipeline(app);
+		app.Run();
+	}
+
+	private static void ConfigureServices(WebApplicationBuilder builder) {
+		// Add services to the container.
+		builder.Services.AddControllers();
+		
+		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+	}
+
+	private static void ConfigurePipeline(WebApplication app) {
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment()) {
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
+
+		app.UseHttpsRedirection();
+
+		app.UseAuthorization();
+
+		app.MapControllers();
+	}
+}
